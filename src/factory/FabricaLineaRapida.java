1da700f967d24b53dae0f4aa3601ffeadead15b2
Jose Bocanegra <josebv107@gmail.com>
2026-07-12 12:00:41 -0500

    feat: implementar FabricaLineaRapida con tasas base

---
+++ src/factory/FabricaLineaRapida.java
@@ -0,0 +1,18 @@
+package factory;
+
+import model.PlanDatos;
+import model.PlanDatosAcademico;
+import model.PlanDatosNegocio;
+import model.PlanDatosPersonal;
+
+public class FabricaLineaRapida extends FabricaCreadora {
+    @Override
+    public PlanDatos crearPlanDatos(String tipo) {
+        switch (tipo) {
+            case "personal": return new PlanDatosPersonal(0.5f);
+            case "negocio": return new PlanDatosNegocio(1.5f);
+            case "academico": return new PlanDatosAcademico(1.0f);
+            default: throw new IllegalArgumentException("Tipo no válido");
+        }
+    }
+}
